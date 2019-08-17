5f9800556883aa6eb1df1ed5e578a2c5df92b675
Anthony Do <anthony4do@gmail.com>
2019-08-17 15:39:51 -0700

    made it better

--- send_sms.js
+++ send_sms.js
@@ -1,6 +1,6 @@
 const accountSid = "AC6601e11e9fedf512b68a3436a9f33867";
 const authToken = "3c2cbfaeaaad0afa3139b8f7571173c2";
-const client = require('twilio')(accountSid, authToken);
+//const client = require('twilio')(accountSid, authToken);
 
 var methods = {
 
