7878753aa69ff0ba3857e191855309d390f0f37f
Anthony Do <anthony4do@gmail.com>
2019-08-18 18:02:02 -0700

    Added .env variables

--- send_sms.js
+++ send_sms.js
@@ -1,5 +1,10 @@
-const accountSid = "AC6601e11e9fedf512b68a3436a9f33867";
-const authToken = "3c2cbfaeaaad0afa3139b8f7571173c2";
+const dotenv = require('dotenv');
+dotenv.config({
+    path: './.env'
+});
+
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
 var methods = {
--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 var express = require('express');
+var dotenv = require('dotenv');
 var bodyParser = require('body-parser')
-var app = express();
 var http = require('http').createServer(app);
-
 
 var databaseHandler = require('./database_module');
 var sendsms = require('./send_sms');
 
+var app = express();
+
+dotenv.config({
+    path: './.env'
+});
 
 const PORT = process.env.PORT || 3000;
-
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
@@ -37,7 +40,7 @@ app.post('/queue', function(req, res) {
 
 
 http.listen(PORT, function() {
-    console.log(`Server running on port 3000`);
+    console.log(`Server running on port ${PORT}`);
 })
 
 function generateTableNumber() {
